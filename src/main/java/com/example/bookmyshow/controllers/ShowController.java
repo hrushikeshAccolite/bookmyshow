16a45083bed3906ce67ae483ddf7605c0c02b5bd
Hrushikesh Sampat Jadhav <hrushikesh.jadhav@accolite.com>
2023-02-20 22:24:41 +0530

    update

--- src/main/java/com/example/bookmyshow/controllers/ShowController.java
+++ src/main/java/com/example/bookmyshow/controllers/ShowController.java
@@ -1,8 +1,14 @@
 package com.example.bookmyshow.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.bookmyshow.models.Show;
 import com.example.bookmyshow.services.ShowServices;
 
 @RestController
@@ -10,5 +16,15 @@ public class ShowController {
     
     @Autowired
     private ShowServices showServices;
+
+    @GetMapping("/show/{showId}")
+    private Show getShowById(@PathVariable long showId) {
+        return this.showServices.getShowById(showId);
+    }
+
+    @PostMapping("/createShow")
+    private ResponseEntity<Show> addShow(@RequestBody Show show){
+        return ResponseEntity.ok().body(this.showServices.createShow(show));
+    }
 
 }
